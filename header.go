18f81b70ca77c2032a877f91ea9d26ba4c827ac0
Mac <mac01@runbox.com>
2021-09-29 23:25:58 +0800

    comments and jheader dct

--- header.go
+++ header.go
@@ -1,15 +1,21 @@
 package main
 
-func jheader(w, h uint16) (out []byte) {
-	// 2 byte unsized size/len
+func jheader(w, h int) (out []byte) {
+	// 2 byte unsized size/length
 	var size []byte
 
 	// jpeg magic byte
 	out = []byte{0xff, 0xd8}
 
 	// Define quantization table(s), len
-	dqt := []byte{0xff, 0xdb, 0x00, 0x0}
+	// B.2.4.1 p39
+	dqt := []byte{
+		0xff, 0xdb, // marker
+		0x00, 0x00, // length
+	}
 	// luminance
+	//   Pq: Quantization table element precision. 0 = 8 bit, 0 = 16 bit
+	//   Tq: Quantization table destination identifier. 0-3
 	lum := *zigZag(QuantizationTableLuminance75())
 	dqt = append(dqt, 0x00) // Pq: 0, Tq: 0
 	dqt = append(dqt, []byte(lum[:])...)
@@ -17,12 +23,38 @@ func jheader(w, h uint16) (out []byte) {
 	chm := *zigZag(QuantizationTableLuminance75())
 	dqt = append(dqt, 0x01) // Pq: 0, Tq: 1
 	dqt = append(dqt, []byte(chm[:])...)
+	// update dqt length
 	size = i16tob(uint16(len(dqt) - 2))[:] // should be 132
-	// update dqt size
 	copy(dqt[2:4], size)
 
+	// height dat
+	sh := i16tob(uint16(h))
+	// width dat
+	sw := i16tob(uint16(w))
+
 	// Baseline DCT, len, precision
-	sof := []byte{0xff, 0xc0, 0x00, 0x00}
+	sof := []byte{
+		0xff, 0xc0, // SOF0: Baseline DCT. marker
+		0x00, 0x00, // Lf: Frame header length
+		0x08,         // P: Sample precision. 8 bit
+		sh[0], sh[1], // Y: height
+		sw[0], sw[1], // X: width
+		0x03, // Nf: Number of image components in frame, hardcoded to 3 for colour
+	}
+	// ref B.2 p36
+	// image component Y
+	sof = append(sof, []byte{
+		0x01, // Ci(8bit): Component identifier
+		0x22, // Hi(4bit): Horizontal sampling factor | Vi(4bit):  Vertical sampling factor
+		0x00, // Tqi(8bit): Quantization table destination selector
+	}...)
+	// image component Cb
+	sof = append(sof, []byte{0x02, 0x11, 0x01}...)
+	// image component Cr
+	sof = append(sof, []byte{0x03, 0x11, 0x01}...)
+	// update sof length
+	size = i16tob(uint16(len(sof)) - 2)[:]
+	copy(sof[2:4], size)
 
 	// Define Huffman table(s), len
 	dht := []byte{0xff, 0xc4, 0x00, 0x00}
